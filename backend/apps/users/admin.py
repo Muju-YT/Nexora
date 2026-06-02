a93c41a2c97602d8abd9e19cae8da4fb051727f3
Muju-YT <muzammil00056@gmail.com>
2026-06-02 11:07:26 +0530

    simplify admin

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -1,37 +1,11 @@
 from django.contrib import admin
 from .models import User, Profile
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from django.contrib.auth.models import Group
-
-admin.site.unregister(Group)
-
-
-class UserAdmin(BaseUserAdmin):
-    model = User
-    list_display = ('email', 'username', 'is_staff', 'is_active')
-    list_filter = ('is_staff', 'is_active')
-    search_fields = ('email', 'username')
-    ordering = ('email',)
-
-    fieldsets = (
-        (None, {'fields': ('email', 'username', 'password')}),
-        ('Permissions', {'fields': ('is_staff', 'is_active')}),
-    )
-
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'username', 'password1', 'password2', 'is_staff', 'is_active')
-        }),
-    )
-
-
-admin.site.register(User, UserAdmin)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("email", "username", "is_staff", "is_active")
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'is_verified', 'created_at')
-    search_fields = ('user__email', 'user__username')
-    exclude = ('followers', 'blocked_users', 'muted_users')
+    list_display = ("user", "is_verified", "created_at")
